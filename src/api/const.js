86431c02e61137f75a29b93570abc4c7c74f61d2
Rafis Shigapov <rafis1work@gmail.com>
2025-08-31 23:53:04 +0300

    changed back to installed app

--- src/api/const.js
+++ src/api/const.js
@@ -1,6 +1,6 @@
 export const URL_API = 'https://oauth.reddit.com';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
-export const CLIENT_ID = 'bHZ0F0xkRzgS67slI10I-A';
+export const CLIENT_ID = 'vFHd1XXw0RofWTRcXZXfXA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
 export const REDIRECT_URI = 'https://blogget-ebon.vercel.app/auth';
