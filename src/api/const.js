d99eb9ca91bb83fc72451ab92139b7e1a4025432
Rafis Shigapov <rafis1work@gmail.com>
2025-08-31 23:41:13 +0300

    change installed app to web app on reddit

--- src/api/const.js
+++ src/api/const.js
@@ -1,6 +1,6 @@
 export const URL_API = 'https://oauth.reddit.com';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
-export const CLIENT_ID = 'DP-CSgbu-LnKcIZOvrQYnw';
+export const CLIENT_ID = 'bHZ0F0xkRzgS67slI10I-A';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
 export const REDIRECT_URI = 'https://blogget-ebon.vercel.app/auth';
