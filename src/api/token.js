d440b180ba48aaa0e38416152c98ed0bcf81cc38
Rafis Shigapov <rafis1work@gmail.com>
2025-09-01 11:26:18 +0300

    fix token

--- src/api/token.js
+++ src/api/token.js
@@ -5,10 +5,7 @@ export const setToken = token => {
 export const getToken = () => {
   let token = localStorage.getItem('bearer') || '';
 
-  if (
-    (!token && location.pathname.includes('/auth')) ||
-    (!token && window.location.hash.startsWith('#access_token='))
-  ) {
+  if (!token && location.pathname.includes('/auth')) {
     token =
       new URLSearchParams(location.hash.substring(1)).get('access_token') || '';
     setToken(token);
